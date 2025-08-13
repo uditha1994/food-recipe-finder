04ed65247a55d71c1ad0406fc9824c94ae68bd96
Uditha Landekumbura <uditha@programmer.net>
2025-08-13 10:11:24 +0530

    searchrecipe and getingredients functions

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,9 @@
+// TheMealDB API
+const BASE_URL = 'https://www.themealdb.com/api/json/v1/1/';
+
 //DOM elements
 const searchInput = document.getElementById('search-input');
-const searchBt = document.getElementById('search-button');
+const searchBtn = document.getElementById('search-button');
 const mealType = document.getElementById('meal-type');
 const dietType = document.getElementById('diet-type');
 const cuisineType = document.getElementById('cuisine-type');
@@ -8,6 +11,74 @@ const resultsContainer = document.getElementById('results-container');
 const loadingElement = document.getElementById('loading');
 const noResultsElement = document.getElementById('no-results');
 const errorElement = document.getElementById('error-message');
+
+//event listeners
+searchBtn.addEventListener('click', searchRecipes);
+searchInput.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        searchRecipes();
+    }
+})
+
+async function searchRecipes() {
+    const query = searchInput.value.trim();
+
+    if (!query) {
+        alert('Please enter a search term');
+        return;
+    }
+
+    showLoading();
+
+    try {
+        let response = await fetch(`${BASE_URL}search.php?s=${query}`);
+        let data = await response.json();
+
+        if (!data.meals || data.meals.length === 0) {
+            //if no results try searching by ingredients
+            response = await fetch(`${BASE_URL}filter.php?i=${query}`);
+            data = await response.json();
+        }
+
+        if (!data.meals || data.meals.length === 0) {
+            displayResults([]);
+            return;
+        }
+
+        const trasformedResults = data.meals.map(meal => ({
+            recipe: {
+                label: meal.strMeal,
+                image: meal.strMealThumb,
+                totalTime: 30,
+                calories: 500,
+                ingredientLines: getIngredientsList(meal),
+                url: meal.strSource || '#'
+            }
+        }));
+
+        displayResults(trasformedResults);
+
+    } catch (error) {
+        showError();
+        console.error('Error fetching recipes: ', error);
+    }
+}
+
+function getIngredientsList(meal) { 
+    const ingredients = [];
+
+    for(let i= 1; i <= 20; i++){
+        const ingredient  = meal[`strIngredient${i}`];
+        const measure = meal[`strMeasure${i}`];
+        if(ingredient && ingredient.trim() !== ''){
+            ingredients.push(`${measure} ${ingredient}`.trim());
+        }
+    }
+
+    return ingredients;
+}
+
+function displayResults(recipes) { }
 
 function showLoading() {
     loadingElement.style.display = 'flex';
@@ -34,5 +105,5 @@ function hideError() {
 
 //initialize
 window.addEventListener('DOMContentLoaded', () => {
-    showLoading();
+
 })
